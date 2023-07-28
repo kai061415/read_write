89b75466dca90358eb92939450ebc1981e396674
kaihan chen <kevinwork061415@gmail.com>
2023-07-28 14:07:11 +0800

    update refactor

--- read.py
+++ read.py
@@ -9,36 +9,53 @@
 import os #operating system
 
 products = []
-if os.path.isfile('test.txt'):
-	print('找到檔案了!')
-	with open('test.txt', 'r', encoding='utf-8') as f:  #讀取檔案
+
+#讀取檔案
+def read_file(filename):
+	with open(filename, 'r', encoding='utf-8') as f:  
 		for line in f:
 			if '體重, 體脂' in line:
 				continue
-			s = line.strip().split(',')
-			products.append(s)
-else:
-	print('找不到檔案!')	
-
+			products.append(line.strip().split(','))	
+	return products
+	
 #讓使用者輸入
-while True:   
-	name = input('請輸入體重: ')
-	if name == 'q':
-		break
-	body = input('請輸入體脂: ')
-	s = []
-	s.append(name)
-	s.append(body)
-	products.append(s)
-print(products)
+def user_input(products):	
+	while True:   
+		name = input('請輸入體重: ')
+		if name == 'q':
+			break
+		body = input('請輸入體脂: ')
+		s = []
+		s.append(name)
+		s.append(body)
+		products.append(s)
+	return products
 
 #印出所有紀錄
-for line in products:
-	print('您的體重為:', line[0], '您的體脂為:', line[1])
-
+def print_products(new_products):
+	for line in products:
+		print('您的體重為:', line[0], '您的體脂為:', line[1])
 
 #寫入檔案
-with open('test.txt', 'w', encoding='utf-8') as f:
-	f.write('體重, 體脂\n')  #新增欄位
-	for line in products:
-		f.write(line[0] + ',' + line[1] + '\n')	
+def write_file(filename, new_products):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('體重, 體脂\n')  #新增欄位
+		for line in new_products:
+			f.write(line[0] + ',' + line[1] + '\n')	
+
+#程式進入點
+def main():
+	products = []
+	filename = 'test.txt'
+	if os.path.isfile(filename):   #檢查檔案是否存在
+		print('找到檔案了!')
+		products = read_file(filename)
+	else:
+		print('找不到檔案!')
+
+	new_products = user_input(products)
+	print_products(new_products)
+	write_file('test.txt', new_products)	
+
+main()	 # refactor 重構		
